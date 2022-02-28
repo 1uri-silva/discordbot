08c2c7f490bf532426caa105e019ccde77dd88aa
Iuri Silva <rd-easy@github.com>
2022-02-28 08:37:43 -0300

    fetch and send file discord bot

---
+++ init.py
@@ -0,0 +1,45 @@
+import json
+import requests
+import discord
+from random import random
+import asyncio
+from dotenv import dotenv_values
+import os
+
+config = dotenv_values(".env")
+
+def fetch_data():
+  x = input('Quantidade de dados: ')
+  res = requests.get('https://randomuser.me/api/?results={}'.format(x))
+  return json.loads(res.text)
+  
+def write_archive():
+  response = fetch_data()
+  name_file = random().__floor__()
+  with open(str(name_file) + '.json', 'w', encoding='utf-8') as f:
+    json.dump(response, f, indent=2)
+  return str(name_file) + '.json'
+
+async def timer():
+  await client.wait_until_ready()
+  channel = client.get_channel(int(config.get('TESTSERVER')))
+  
+  while True:
+    file = write_archive()
+    await channel.send('@everyone', file=discord.File(file))
+    await asyncio.sleep(10) #seconds
+    
+    if os.path.exists(file):
+      os.remove(file)
+    else:
+      print("The file does not exist")
+      
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Estou online')
+  await timer()
+
+client.run(config.get('TOKEN'))
+    
